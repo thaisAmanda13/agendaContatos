b3bc44de525448d9a2b995e0337cb8be8be3ea30
Thais Amanda <thais.amandas18@gmail.com>
2021-08-09 14:13:05 -0300

    Implementar contato class

---
+++ src/app/class/contato.ts
@@ -0,0 +1,53 @@
+import { TemplateBindingParseResult } from "@angular/compiler"
+
+export class Contato {
+
+    private _nome: string
+    private _telefone: number
+    private _dataNascimento: string
+    private _sexo: string
+
+    constructor(nome: string, telefone: number, dataNascimento: string, sexo: string) {
+        this._nome = nome
+        this._telefone = telefone
+        this._sexo = sexo
+        this._dataNascimento = dataNascimento
+    }
+
+    public getNome(): string {
+        return this._nome
+    }
+
+    public setNome(nome: string): void {
+        this._nome = nome
+    }
+
+    public setTelefone(telefone: number): void {
+        this._telefone = telefone
+    }
+
+    public getTelefone() 
+    {
+        return this._telefone
+    }
+
+    public getSexo(): string 
+    {
+        return this._sexo
+    }
+
+    public setSexo(sexo: string):void 
+    {
+        this._sexo = sexo
+    }
+
+    public getDataNascimento(): string 
+    {
+        return this._dataNascimento
+    }
+
+    public setDataNascimento(dataNascimento: string): void 
+    {
+        this._dataNascimento = dataNascimento
+    }
+}
